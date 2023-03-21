e958c48182507ac7da5e641b4653d8a62517d6b5
Константин Шперлинг <Puma-Yellow@yandex.ru>
2023-03-21 16:51:57 +0500

    Вторая часть проекта

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,7 +88,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_backend/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_backend')
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
